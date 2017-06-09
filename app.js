04a59a9234e7492d39b7c889a93706a922d59d62
Boris Pruessmann <boris@pruessmann.org>
2017-06-09 23:14:20 +0200

    Updated version to 0.0.2

--- app.js
+++ app.js
@@ -82,7 +82,7 @@ var harmonyDiscovery = new HarmonyDiscovery();
 var roon = new RoonApi({
     extension_id:        'org.pruessmann.roon.logitech.harmony',
     display_name:        'Logitech Harmony',
-    display_version:     '0.0.1',
+    display_version:     '0.0.2',
     publisher:           'Doc Bobo',
     email:               'boris@pruessmann.org',
     website:             'https://github.com/docbobo/roon-extension-harmony',
